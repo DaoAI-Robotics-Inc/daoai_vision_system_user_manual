6771a641e2de0d9c9a2816edfb5ed410292af984
Tom Zhang <tzhang@daoai.com>
2023-06-01 12:31:16 -0700

    update build conifg

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,6 +40,7 @@ html_static_path = ['_static']
 
 # -- Options for HTML output
 language = 'zh_CN'
+html_search_language = 'zh'
 
 html_theme = 'sphinx_rtd_theme'
 
@@ -47,8 +48,7 @@ html_theme = 'sphinx_rtd_theme'
 epub_show_urls = 'footnote'
 
 # -- Options to Support pdf build in chinese
-latex_engine = 'xelatex'
-latex_use_xindy = False
+latex_engine = 'lualatex'
 latex_elements = {
     'preamble': '\\usepackage[UTF8]{ctex}\n',
 }
